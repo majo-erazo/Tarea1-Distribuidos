bf332acf1e3cfb411ffa1dd9857125424ae77860
nikkkaido <86696773+nikkkaido@users.noreply.github.com>
2022-09-18 01:21:47 -0300

    Add files via upload

---
+++ grpc_server/server.js
@@ -0,0 +1,33 @@
+const grpc = require("@grpc/grpc-js");
+const PROTO_PATH = "./search.proto";
+const protoLoader = require("@grpc/proto-loader");
+
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+const packageDef= protoLoader.loadSync(PROTO_PATH, options);
+const objProto = grpc.loadPackageDefinition(packageDef);
+
+function main() {
+  const server = new grpc.Server();
+  server.addService(objProto.Search.service, {
+    getLinks: (_, callback) => {
+      const linksName = _.request.message;
+      const link = products.products_list.filter(({ name }) => name.includes(linksName));
+      callback(null, { link: link});
+    }
+  });
+  server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), (err, port) => {
+    if (err != null) console.log(err);
+    else {
+      console.log("GRPC RUN AT http://localhost:50051");
+      server.start();
+    }
+  });
+}
+
+main();
