77c25b6ae3ac0209445c151920c83a2a0b59a0f3
majo-erazo <75627494+majo-erazo@users.noreply.github.com>
2022-09-18 21:56:02 -0400

    .

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 ## Caché y RPC
 En este repositorio se tendra el codigo y las instrucciones para poder ejecutar la tarea 1.
 ### Primeros Pasos:
+Se debe descargar desde el [Colecciones de AOL de usuarios](http://www.cim.mcgill.ca/~dudek/206/Logs/AOL-user-ct-collection/) (Se utilizo la coleccion 07 para el video y la implementación).
 
-```
-
-```
 ### Comandos a ejecutar
 Como se utilizara docker para la implementacion de esto, basta con ejecutar el siguiente comando:
 ```
--- backend/redis.js
+++ backend/redis.js
@@ -1,11 +1,23 @@
-const redis = require("redis");
-const client = redis.createClient({
-  host: 'redis1', // grande docker
-  port: 6379,
+const redis  = require('redis');
+
+const cluster = redis.createCluster({
+  rootNodes: [
+    {
+      url: 'redis://redis1:6379'
+    },
+    {
+      url: 'redis://redis2:6380'
+    },
+    {
+      url: 'redis://redis3:6381'
+    }
+  ]
 });
 
-client.on("error", (err) => {
-  console.log("Error " + err);
-});
+cluster.on('error', (err) => console.log('Redis Cluster Error', err));
 
-module.exports = client;
+const init = async () => {
+  await cluster.connect();
+}
+init();
+module.exports = cluster;
